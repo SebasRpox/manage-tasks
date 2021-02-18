6fba960cd62ec72d2ea56185cd506430bd37377e
SebasRpox <sebaxrpo@gmail.com>
2021-02-18 08:52:59 -0500

    footer

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer>
-    <hr />
-    <p>Footer</p>
-  </footer>
+  <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Gestor de Tareas</h5>
+                <p class="grey-text text-lighten-4">Esta aplicación fue desarrollado con el propósito de organizar tu horario disfrutala!</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Gestor de Tareas. Todos los derechos reservados.
+            <a class="grey-text text-lighten-4 right" href="#!">MIT License</a>
+            </div>
+          </div>
+        </footer>
 );
 
 export default Footer;
